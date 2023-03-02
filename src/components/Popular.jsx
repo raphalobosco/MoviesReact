ffd81d42e7fa8364c03258e6d71ad35a1098b2eb
Rapha Lobosco <83170908+raphacomics@users.noreply.github.com>
2023-03-02 00:14:03 +0000

    Adding featured movie

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -9,6 +9,7 @@ function Popular() {
 
 
     const [popular, setPopular] = useState([])
+    const [featured, setFeatured] = useState([])
 
     // const options = {
     //     method: 'GET',
@@ -18,22 +19,50 @@ function Popular() {
     //     }
     // };
 
-    const getPopular = async () => {
+    const getMovies = async () => {
         const api = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_REACT_APP_MOVIE_API}&language=en-US&page=1&region=US`)
         const data = await api.json()
         // console.log(data)
         // console.log(data.name)
         setPopular(data.results)
+        setFeatured([data.results[Math.floor(Math.random() * 20)]]);
+
     };
 
     useEffect(() => {
-        getPopular();
+        getMovies();
     }, [])
 
-    console.log(popular)
+    console.log(featured)
+    // console.log(popular)
 
     return (
         <div>
+
+            <div>
+                {featured.map(movie => {
+                    return (
+                        <div className="movieFeat" key={movie.id} >
+                            <img src={"https://image.tmdb.org/t/p/w1280" + movie.backdrop_path} className="backImg" alt="" />
+                            <img src={"https://image.tmdb.org/t/p/w500" + movie.poster_path} alt="" className="moviePoster" />
+                            <div className="movieInfo">
+                                <p className="feature">Featured Movie</p>
+                                <p className="title">{movie.original_title}</p>
+                                <p className="overview"> {movie.overview} </p>
+
+                                <div className="playTrailer">
+                                    <button className="trailer-btn">P</button>
+                                    <span>Play Trailer</span>
+                                </div>
+                            </div>
+
+                        </div>
+                    )
+                })}
+
+
+            </div>
+
             <div className="wrapper">
                 <h3>Popular Movies</h3>
 
@@ -43,13 +72,16 @@ function Popular() {
                     gap: '1rem',
                     pagination: false,
                     drag: 'free',
+                    trimSpace: 'move',
+                    focus: 'center',
                     breakpoints: {
                         768: {
                             perPage: 4
                         },
                         425: {
-                            fixedWidth: '8rem',
-                            gap: '.5rem'
+                            fixedWidth: '9rem',
+                            gap: '.5rem',
+                            arrows: false,
                         }
                     }
                 }} >
