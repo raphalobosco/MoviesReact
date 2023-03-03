d9acfd9cf9ca4a4ff2df4a6db9e9636c3f73d838
Rapha Lobosco <raphacomics@gmail.com>
2023-03-03 09:58:04 +0000

    Update Discover.jsx

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,53 +1,58 @@
-import { Link, useParams } from 'react-router-dom'
-import { useEffect, useState } from 'react';
-import { FaStar } from 'react-icons/fa'
-import styled from 'styled-components';
-
-
+import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { FaStar } from "react-icons/fa";
+import styled from "styled-components";
 
 function Discover() {
+  const [genres, setGenres] = useState([]);
 
-  const [genres, setGenres] = useState([])
-
-  let params = useParams()
+  let params = useParams();
 
   const getMovies = async (genre) => {
-    const api = await fetch(`https://api.themoviedb.org/3/discover/movie/?api_key=${import.meta.env.VITE_REACT_APP_MOVIE_API}&language=en-US&page=1&region=US&with_original_language=en&with_genres=${genre}/`)
+    const api = await fetch(
+      `https://api.themoviedb.org/3/discover/movie/?api_key=${
+        import.meta.env.VITE_REACT_APP_MOVIE_API
+      }&language=en-US&page=1&region=US&with_genres=${genre}&with_original_language=en`
+    );
 
-    const movies = await api.json()
+    const movies = await api.json();
 
-    setGenres(movies.results)
-    console.log(genres)
+    setGenres(movies.results);
+    console.log(genres);
   };
 
   useEffect(() => {
-    getMovies(params.type)
-  }, [params.type])
+    getMovies(params.type);
+  }, [params.type]);
 
   return (
     <Grid>
-      {genres.map(movie => {
+      {genres.map((movie) => {
         return (
-
-          <div className="movieCard" key={movie.id} style={{ backgroundImage: `url("https://image.tmdb.org/t/p/w500${movie.poster_path}")`, backgroundSize: 'cover' }} >
-
-
-            <span><FaStar /> {movie.vote_average}</span>
+          <div
+            className="movieCard"
+            key={movie.id}
+            style={{
+              backgroundImage: `url("https://image.tmdb.org/t/p/w500${movie.poster_path}")`,
+              backgroundSize: "cover",
+            }}
+          >
+            <span>
+              <FaStar /> {movie.vote_average}
+            </span>
             <p>{movie.original_title}</p>
             <div className="gradient"></div>
           </div>
-        )
-
+        );
       })}
     </Grid>
-  )
+  );
 }
-
 
 const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
-  gap:1rem
-`
+  gap: 1rem;
+`;
 
-export default Discover
+export default Discover;
