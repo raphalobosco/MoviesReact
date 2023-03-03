5a59e040a3d5be4fc86f72d8aeb24ce74e8d9639
Rapha Lobosco <raphacomics@gmail.com>
2023-03-03 14:43:53 +0000

    Update 3 files

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,53 +1,53 @@
-import { NavLink } from 'react-router-dom'
-import styled from 'styled-components'
-import { useState, useEffect } from 'react';
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import { useState, useEffect } from "react";
 
 function Categories() {
+  const [genres, setGenres] = useState([]);
 
-    const [genres, setGenres] = useState([])
+  useEffect(() => {
+    async function fetchData() {
+      const api = await fetch(
+        `https://api.themoviedb.org/3/genre/movie/list?api_key=${
+          import.meta.env.VITE_REACT_APP_MOVIE_API
+        }&language=en-US&page=1&region=US`
+      );
+      const data = await api.json();
+      setGenres(data.genres);
+    }
+    fetchData();
+  }, []);
 
-
-    useEffect(() => {
-
-        async function fetchData() {
-            const api = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${import.meta.env.VITE_REACT_APP_MOVIE_API}&language=en-US`)
-            const data = await api.json()
-            setGenres(data.genres)
-        }
-        fetchData()
-    }, [])
-
-
-    return (
-        <div>
-            <List>
-                <NavLink to={`/`}>Home</NavLink>
-                {genres.map((genre) =>
-                    genre.name === "Adventure" ? (
-                        <NavLink key={genre.id} to={`/discover/${genre.id}`}>
-                            Adventure
-                        </NavLink>
-                    ) : genre.name === "Comedy" ? (
-                        <NavLink key={genre.id} to={`/discover/${genre.id}`}>
-                            Comedy
-                        </NavLink>
-                    ) : genre.name === "Drama" ? (
-                        <NavLink key={genre.id} to={`/discover/${genre.id}`}>
-                            Drama
-                        </NavLink>
-                    ) : genre.name === "Thriller" ? (
-                        <NavLink key={genre.id} to={`/discover/${genre.id}`}>
-                            Thriller
-                        </NavLink>
-                    ) : null
-                )}
-            </List>
-        </div >
-    )
+  return (
+    <div>
+      <List>
+        <NavLink to={`/`}>Home</NavLink>
+        {genres.map((genre) =>
+          genre.name === "Adventure" ? (
+            <NavLink key={genre.id} to={`/discover/${genre.id}/`}>
+              Adventure
+            </NavLink>
+          ) : genre.name === "Comedy" ? (
+            <NavLink key={genre.id} to={`/discover/${genre.id}/`}>
+              Comedy
+            </NavLink>
+          ) : genre.name === "Drama" ? (
+            <NavLink key={genre.id} to={`/discover/${genre.id}/`}>
+              Drama
+            </NavLink>
+          ) : genre.name === "Thriller" ? (
+            <NavLink key={genre.id} to={`/discover/${genre.id}/`}>
+              Thriller
+            </NavLink>
+          ) : null
+        )}
+      </List>
+    </div>
+  );
 }
 
-
-{/* <NavLink to={'/genres/&with_genres=35'}>Comedy</NavLink>
+{
+  /* <NavLink to={'/genres/&with_genres=35'}>Comedy</NavLink>
                 <NavLink to={'&with_genres=18'}>Drama</NavLink>
                 <NavLink to={'&with_genres=12'}>Adventure</NavLink>
                 <NavLink to={'&with_genres=53'}>Thriller</NavLink> 
@@ -55,16 +55,15 @@ function Categories() {
                 <NavLink to={'/discover/drama'}>Drama</NavLink>
                 <NavLink to={'/discover/adventure'}>Adventure</NavLink>
                 <NavLink to={'/discover/thriller'}>Thrilles</NavLink>
-                */}
+                */
+}
 
 const List = styled.ul`
-    display:flex;
-    gap: 3rem;
-    width: 100%;
-    justify-content: center;
-    margin-block: 2rem;
+  display: flex;
+  gap: 3rem;
+  width: 100%;
+  justify-content: center;
+  margin-block: 2rem;
+`;
 
-    
-`
-
-export default Categories
+export default Categories;
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -10,9 +10,9 @@ function Discover() {
 
   const getMovies = async (genre) => {
     const api = await fetch(
-      `https://api.themoviedb.org/3/discover/movie/?api_key=${
+      `https://api.themoviedb.org/3/discover/movie?api_key=${
         import.meta.env.VITE_REACT_APP_MOVIE_API
-      }&language=en-US&page=1&region=US&with_genres=${genre}&with_original_language=en`
+      }&with_genres=${genre}`
     );
 
     const movies = await api.json();
