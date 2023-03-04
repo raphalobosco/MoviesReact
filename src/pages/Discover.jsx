5c6ca39dfa1fb0deb9e757bd4d6f4c01738f3514
Rapha Lobosco <83170908+raphacomics@users.noreply.github.com>
2023-03-04 23:58:54 +0000

    page fix

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { FaStar } from "react-icons/fa";
 import styled from "styled-components";
